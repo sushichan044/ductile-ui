51be231d54e46cd3d10b039be7a3bb6936a3d4a6
sushi-chaaaan <mail@sushichan.live>
2023-10-04 14:53:17 +0900

    feat: implement draft version

---
+++ src/ductile/controller/messageable_controller.py
@@ -0,0 +1,17 @@
+import discord
+
+from src.ductile import View
+
+from .controller import ViewController
+
+
+class MessageableController(ViewController):
+    def __init__(self, view: View, *, messageable: discord.abc.Messageable, timeout: float | None = 180) -> None:
+        super().__init__(view, timeout=timeout)
+        self.__messageable = messageable
+
+    async def send(self) -> None:
+        target = self.__messageable
+        view_kwargs = self._process_view_for_discord("files")
+
+        self.message = await target.send(**view_kwargs)
