7128387470f6ea5a50c0d26b1b7c8948221de21f
sushi-chaaaan <mail@sushichan.live>
2024-06-24 01:10:57 +0900

    Add sync internval option to ViewController Fixes #90

--- src/ductile/utils/__init__.py
+++ src/ductile/utils/__init__.py
@@ -1,14 +1,16 @@
 from .async_helper import get_all_tasks, wait_tasks_by_name
 from .call import call_any_function
 from .chunk import chunks
+from .debounce import debounce
 from .logger import get_logger
 from .type_helper import is_async_func, is_sync_func
 
 __all__ = [
-    "chunks",
     "get_all_tasks",
     "wait_tasks_by_name",
     "call_any_function",
+    "chunks",
+    "debounce",
     "get_logger",
     "is_async_func",
     "is_sync_func",
