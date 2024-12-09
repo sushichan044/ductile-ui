b118a088726cb60a29f101cf539bbb61daf74e9a
sushichan044 <mail@sushichan.live>
2024-12-09 13:31:38 +0900

    add test for state

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,14 +1,29 @@
 from collections.abc import Generator
 
 import pytest
+from pytest_mock import MockFixture, MockType
 
 from ductile import State
 from ductile.view import View
 
 
 @pytest.fixture
-def state() -> Generator[State[int], None, None]:
-    test_state = State(0, View())
+def view() -> View:
+    return View()
+
+
+@pytest.fixture
+def view_spy(mocker: MockFixture, view: View) -> Generator[MockType, None, None]:
+    spy = mocker.spy(view, "sync")
+
+    yield spy
+    spy.reset_mock()
+
+
+@pytest.fixture
+def state(view: View) -> Generator[State[int], None, None]:
+    test_state = State(0, view)
+
     yield test_state
     test_state.set_state(0)
 
@@ -24,6 +39,11 @@ def test_get_state_with_call(state: State[int]) -> None:
 def test_set_state(state: State[int]) -> None:
     state.set_state(1)
     assert state.get_state() == 1
+
+
+def test_set_state_bubble_sync(state: State[int], view_spy: MockType) -> None:
+    state.set_state(1)
+    view_spy.assert_called_once()
 
 
 def test_set_state_with_previous_value(state: State[int]) -> None:
